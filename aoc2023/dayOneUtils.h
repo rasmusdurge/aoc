5d5170f6111144b0f7e3258c5a319f153ab68968
rasmus <rasmus.durge@gmail.com>
2026-02-08 16:43:03 +0100

    refactor

--- aoc2023/dayOneUtils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-#include <map>
-std::string getFirstAndLastChar(const std::string& inputString); 
-std::string getNumbersAsString(const std::string& str);
-std::string translateString(const std::string& inputString, const std::map<std::string,std::string>& numberMap);
