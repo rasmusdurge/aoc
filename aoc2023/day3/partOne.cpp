faa8829bbf45c70ae5765d70db6fecc21c6758bf
rasmus <rasmus.durge@gmail.com>
2026-05-05 16:50:43 +0200

    added day3

---
+++ aoc2023/day3/partOne.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Add up all the part numbers
+// If its not next to a number or dot 
+
+int main() {
+
+    // Look for symobls. Look for everything around the symbol.
+    
+}
